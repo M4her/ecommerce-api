8cc427d5908152b0f2757362cabb3553db05fceb
M4her <maher44hmed@gmail.com>
2026-05-12 00:00:10 +0600

    E-commerce start

---
+++ controllers/authControllers.js
@@ -0,0 +1,5 @@
+const signUp = async (req, res) => {
+  console.log("Registration");
+};
+
+module.exports = { signUp };
---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+const router = require("./route")
+
+app.use(router)
+
+app.listen(8000, () => {
+  console.log("Server is running.");
+});
